ace4197018c9ff00f7a178a75178897322d7147b
Eduardo <eduardo.robettibedin@gmail.com>
2026-02-18 15:07:43 -0300

    OOP: add printPerson() to Person class and read input for two instances

--- app/src/Main.kt
+++ app/src/Main.kt
@@ -5,10 +5,10 @@ fun main() {
     person.name = readLine() ?: ""
     println("Insert your age: ")
     person.age = readLine()?.toInt() ?: 0
-    println("Your name is ${person.name} and you are ${person.age} years old")
+    person.printPerson()
     println("Insert your name: ")
     person2.name = readLine() ?: ""
     println("Insert your age: ")
     person2.age = readLine()?.toInt() ?: 0
-    println("Your name is ${person2.name} and you are ${person2.age} years old")
+    person2.printPerson()
 }
--- app/src/Person.kt
+++ app/src/Person.kt
@@ -1,4 +1,8 @@
 class Person {
   var name: String = ""
   var age: Int = 0
+
+  fun printPerson() {
+    println("Your name is $name and you are $age years old")
+  }
 }
