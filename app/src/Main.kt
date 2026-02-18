29121f16c090236b506d525e06f963038fe79c65
Eduardo <eduardo.robettibedin@gmail.com>
2026-02-18 15:32:19 -0300

    OOP: add ProductCard class with init() and printProductCard(), read product data in main

--- app/src/Main.kt
+++ app/src/Main.kt
@@ -1,16 +1,33 @@
 fun main() {
-    val person = Person()
-    val person2 = Person()
-    println("Insert your name: ")
-    person.name = readLine() ?: ""
-    println("Insert your age: ")
-    person.age = readLine()?.toInt() ?: 0
-    person.printPerson()
-    println("Insert your name: ")
-    person2.name = readLine() ?: ""
-    println("Insert your age: ")
-    person2.age = readLine()?.toInt() ?: 0
-    person2.printPerson()
-    val assistant = Assistant()
-    assistant.bringCoffee(3, "Coffee")
+    // val person = Person()
+    // val person2 = Person()
+    // println("Insert your name: ")
+    // person.name = readLine() ?: ""
+    // println("Insert your age: ")
+    // person.age = readLine()?.toInt() ?: 0
+    // person.printPerson()
+    // println("Insert your name: ")
+    // person2.name = readLine() ?: ""
+    // println("Insert your age: ")
+    // person2.age = readLine()?.toInt() ?: 0
+    // person2.printPerson()
+    // val assistant = Assistant()
+    // assistant.bringCoffee(3, "Coffee")
+    var name: String 
+    var brand: String 
+    var size: Float 
+    var price: Int 
+
+    println("Insert the name of the product: ")
+    name = readLine() ?: ""
+    println("Insert the brand of the product: ")
+    brand = readLine() ?: ""
+    println("Insert the size of the product: ")
+    size = readLine()?.toFloat() ?: 0f
+    println("Insert the price of the product: ")
+    price = readLine()?.toInt() ?: 0
+
+    val productCard = ProductCard()
+    productCard.init(name, brand, size, price)
+    productCard.printProductCard()
 }
--- app/src/Person.kt
+++ app/src/Person.kt
@@ -2,6 +2,11 @@ class Person {
   var name: String = ""
   var age: Int = 0
 
+  fun init(name: String, age: Int) {
+    this.name = name
+    this.age = age
+  }
+
   fun printPerson() {
     println("Your name is $name and you are $age years old")
   }
